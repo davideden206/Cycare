48f6b2f38f4b40709d27e559fcaf2c2be1841942
davideden206 <46527070+davideden206@users.noreply.github.com>
2019-04-24 15:16:06 +0300

    connect sdk

--- app/src/main/java/david/eden/yishi/corsh/ActionActivity.java
+++ app/src/main/java/david/eden/yishi/corsh/ActionActivity.java
@@ -2,6 +2,7 @@ package david.eden.yishi.corsh;
 
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,25 +10,52 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ioteratech.blueterasdk.BlueteraCallbacks;
 import com.ioteratech.blueterasdk.BlueteraManager;
 import com.ioteratech.blueterasdk.data.Quaternion;
 import com.ioteratech.blueterasdk.data.Vector3D;
 
-public class ActionActivity extends AppCompatActivity implements BlueteraCallbacks {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ActionActivity extends AppCompatActivity implements BlueteraCallbacks, View.OnClickListener {
 
     private BlueteraManager mBlueteraManager;
     private TextView mStatusTextView;
     private Button mConnectButton;
     private Button mDisconnectButton;
-
+    private Button btn1 ,btn2,btn3,btn4,btn5,btn6,btn7;
+    private String data;
+    private Quaternion CurentQuaternion;
+    private Vector3D Accelerometer;
+    FileOutputStream out;
+    InputStream in;
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        btn4 = (Button)findViewById(R.id.btn4);
+        btn5 = (Button)findViewById(R.id.btn5);
+        btn6 = (Button)findViewById(R.id.btn6);
+        btn7 = (Button)findViewById(R.id.btn7);
 
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
 
         mBlueteraManager = new BlueteraManager(getApplication());
         mBlueteraManager.setGattCallbacks(this);
@@ -44,13 +72,28 @@ public class ActionActivity extends AppCompatActivity implements BlueteraCallbac
         connecting ();
     }
 
+
+    public void writedata(Button btn){
+        for (int i = 0;i<50;i++){
+            data+="\n"+btn.getText()+"Quaternion: W = "+String.valueOf(CurentQuaternion.getW())+" X = "+String.valueOf(CurentQuaternion.getX())
+                    +" Y = "+String.valueOf(CurentQuaternion.getY())+" Z = "+String.valueOf(CurentQuaternion.getZ())+
+                    " Accelerometer: X = "+String.valueOf(Accelerometer.getX())+" Y = "+String.valueOf(Accelerometer.getY())+" Z = "+String.valueOf(Accelerometer.getZ());
+        }
+        Toast.makeText(this, "סיים", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public void onAccelerometerData(BluetoothDevice bluetoothDevice, int i, Vector3D vector3D) {
 
+        this.Accelerometer = vector3D;
     }
 
     @Override
     public void onQuaternionData(BluetoothDevice bluetoothDevice, int i, Quaternion quaternion) {
+        Log.i("tag", "X" + quaternion.getX());
+        Log.i("tag", "Y" + quaternion.getY());
+        Log.i("tag", "Z" + quaternion.getZ());
+        this.CurentQuaternion = quaternion;
 
     }
 
@@ -99,7 +142,7 @@ public class ActionActivity extends AppCompatActivity implements BlueteraCallbac
         Log.d("tag", "device ready");
         //mStatusTextView.setText("Device Ready");
         mBlueteraManager.enableAccelerometer(true);
-        //mBlueteraManager.enableQuaternion(true);
+        mBlueteraManager.enableQuaternion(true);
         //mDisconnectButton.setEnabled(true);
     }
 
@@ -137,6 +180,44 @@ public class ActionActivity extends AppCompatActivity implements BlueteraCallbac
                 .retry(3, 100)
                 .useAutoConnect(false)
                 .enqueue();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btn7) {
+
+                 str = this.data;
+
+                 File file = new File(Environment.DIRECTORY_DOWNLOADS,"cycare.txt");
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(str.getBytes());
+                fos.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else if(v==btn1){
+            writedata(btn1);
+        }
+        else if(v==btn2){
+            writedata(btn2);
+        }
+        else if(v==btn3){
+            writedata(btn3);
+        }
+        else if(v==btn4){
+            writedata(btn4);
+        }
+        else if(v==btn5){
+            writedata(btn5);
+        }
+        else if(v==btn6){
+            writedata(btn6);
+        }
 
     }
 }
